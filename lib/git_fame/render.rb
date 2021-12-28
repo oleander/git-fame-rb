314160cda5645060aac69de280095de612acca78
Linus Oleander <oleander@users.noreply.github.com>
2021-12-28 01:34:15 +0100

    Move contribution render engine into refinement

--- lib/git_fame/render.rb
+++ lib/git_fame/render.rb
@@ -8,51 +8,30 @@ module GitFame
   class Render < Base
     FIELDS = [:name, :email, :lines, :commits, :files, :dist].map(&:to_s).freeze
 
-    Table = Struct.new(:name, :email, :lines)
     attribute :branch, Types::String
     attribute :result, Result
     delegate_missing_to :result
+
+    using Module.new {
+      refine Contribution do
+        def dist(result)
+          l = lines.to_f / result.lines
+          c = commits.count.to_f / result.commits.count
+          f = files.count.to_f / result.files.count
+
+          "%0.1f%% / %0.1f%% / %0.1f%%" % [l * 100, c * 100, f * 100]
+        end
+      end
+    }
 
     def call
       table = TTY::Table.new(header: FIELDS)
 
       contributions.map do |c|
-        table << [c.name, c.email, c.lines, c.commits.count, c.files.count, dist(c)]
+        table << [c.name, c.email, c.lines, c.commits.count, c.files.count, c.dist(self)]
       end
 
       print table.render(:unicode, width: TTY::Screen.width, resize: true, alignment: [:center])
-    end
-
-    private
-
-    def dist(contribution)
-      l = contribution.lines.to_f / lines
-      c = contribution.commits.count.to_f / commits.count
-      f = contribution.files.count.to_f / files.count
-
-      "%0.1f%% / %0.1f%% / %0.1f%%" % [l * 100, c * 100, f * 100]
-    end
-
-    def files_count
-      number_with_delimiter(files.count)
-    end
-
-    def contributions_count
-      contributions.count
-    end
-
-    def commits_count
-      number_with_delimiter(commits.count)
-    end
-
-    def lines_count
-      number_with_delimiter(lines)
-    end
-
-    def table_authors
-      contributions.map do |c|
-        Table.new(c.name, c.email, number_with_delimiter(c.lines))
-      end
     end
   end
 end
